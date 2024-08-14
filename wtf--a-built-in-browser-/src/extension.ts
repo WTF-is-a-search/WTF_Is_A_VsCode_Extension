a872a45c3a55520bfa3011964cd37a976d75fbe6
Elbert-Ainstein <iwu48@yahoo.com>
2024-08-14 15:54:09 +0800

    Added a WebviewPanel Opener with updater, buggy

--- wtf--a-built-in-browser-/src/extension.ts
+++ wtf--a-built-in-browser-/src/extension.ts
@@ -1,6 +1,9 @@
 // The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
+
+
+import { openWebView } from './functions/openWebView';
+import CatCodingPanel from './classes/CatCodingPanel';
 
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
@@ -13,13 +16,29 @@ export function activate(context: vscode.ExtensionContext) {
 	// The command has been defined in the package.json file
 	// Now provide the implementation of the command with registerCommand
 	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('wtf--a-built-in-browser-.helloWorld', () => {
+	const disposable = vscode.commands.registerCommand('wtf.helloWorld', () => {
 		// The code you place here will be executed every time your command is executed
 		// Display a message box to the user
 		vscode.window.showInformationMessage('Hello World from WTF');
 	});
 
 	context.subscriptions.push(disposable);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('wtf.start', () => {
+			CatCodingPanel.createOrShow(context.extensionUri);
+		})
+	);
+
+	context.subscriptions.push(
+		vscode.commands.registerCommand('wtf.doRefactor', () => {
+			if (CatCodingPanel.currentPanel) {
+				CatCodingPanel.currentPanel.doRefactor();
+			}
+		})
+	);
+
+	openWebView(context);
 }
 
 // This method is called when your extension is deactivated
---
+++ wtf--a-built-in-browser-/src/functions/openWebView.ts
@@ -0,0 +1,44 @@
+import * as vscode from 'vscode';
+import CatCodingPanel from '../classes/CatCodingPanel';
+
+const cats = {
+	'Coding Cat': 'https://media.giphy.com/media/JIX9t2j0ZTN9S/giphy.gif',
+	'Compiling Cat': 'https://media.giphy.com/media/mlvseq9yvZhba/giphy.gif',
+	'Testing Cat': 'https://media.giphy.com/media/3oriO0OEd9QIDdllqo/giphy.gif'
+};
+
+export function openWebView(context: vscode.ExtensionContext) {
+
+
+	if (vscode.window.registerWebviewPanelSerializer) {
+		// Make sure we register a serializer in activation event
+		vscode.window.registerWebviewPanelSerializer(CatCodingPanel.viewType, {
+			async deserializeWebviewPanel(webviewPanel: vscode.WebviewPanel, state: any) {
+				console.log(`Got state: ${state}`);
+				// Reset the webview options so we use latest uri for `localResourceRoots`.
+				webviewPanel.webview.options = getWebviewOptions(context.extensionUri);
+				CatCodingPanel.revive(webviewPanel, context.extensionUri);
+			}
+		});
+	}
+}
+
+export function getWebviewOptions(extensionUri: vscode.Uri): vscode.WebviewOptions {
+	return {
+		// Enable javascript in the webview
+		enableScripts: true,
+
+		// And restrict the webview to only loading content from our extension's `media` directory.
+		localResourceRoots: [vscode.Uri.joinPath(extensionUri, 'media')]
+	};
+}
+
+export function getNonce() {
+	let text = '';
+	const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+	for (let i = 0; i < 32; i++) {
+		text += possible.charAt(Math.floor(Math.random() * possible.length));
+	}
+	return text;
+}
+ 
